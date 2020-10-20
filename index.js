05a4bc98b7e790957ecbc9ae4210f510f85c7623
kittybagpipes <sparticus978@gmail.com>
2020-10-20 08:25:08 -0700

    removed spaggetti code and created render list function

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-function main () {
-    $('#js-shopping-list-form').on('submit', function(event) {
-        event.preventDefault();
-        let entry = $('#shopping-list-entry').val();
-        let newListItem =       
-        `<li>
-        <span class="shopping-item">${entry}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`
+const store = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: false},
+  {name: 'bread', checked: false}
+];
 
-      $('.shopping-list').append(newListItem);
-      $('#shopping-list-entry').val('');
-    });
-
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(event.currentTarget).parent().parent().children('.shopping-item').toggleClass('shopping-item__checked');
-
-      });
-
-      $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(event.currentTarget).parent().parent().remove();
-
-      });
+function renderShoppingList() {
+  let html=''
+  for(let i = 0; i < store.length; i++) {
+    html += `<li>
+    <span class="shopping-item">${store.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`
+  }
 }
 
-$(main);
+function main () {
+
+}
+$(main)
