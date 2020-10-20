a973ed0a3b66cdbafe0c76363f1fd595bd43ed14
kittybagpipes <sparticus978@gmail.com>
2020-10-20 08:41:58 -0700

    Built Skeleton for Shopping list using Psudocode, removed all spaggetti code

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ const store = [
 ];
 
 function renderShoppingList() {
+  // Responsible for rendering shopping list in DOM
   let html=''
   for(let i = 0; i < store.length; i++) {
-    html += `<li>
-    <span class="shopping-item">${store.name}</span>
-    <div class="shopping-item-controls">
-      <button class="shopping-item-toggle">
-        <span class="button-label">check</span>
-      </button>
-      <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-      </button>
-    </div>
-  </li>`
+    html += generateListItem(store[i])
   }
 }
 
-function main () {
+function generateListItem(item) {
+  // Responsible for generating HTML from store variable
+  return `<li>
+  <span class="shopping-item">${item.name}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`
+}
+
+function handleNewItemSubmit(){
+  // Responsible for adding a new item to the shopping list
 
 }
-$(main)
+
+function handleItemCheckClicked() {
+  // This function will handle when Check item is clicked and it needs to be checked off.
+
+}
+ function handleDeleteItemClicked() {
+  // This function will handle deleting items from shopping cart one by one
+
+ }
+
+function handleShoppingList () {
+  //This will hold all callback functions for document ready
+
+}
